2b3e3f6aa93792098cfdf33a9de95eaa28ec2613
Douglas Feaster <Douglas.Feaster@johnsonu.edu>
2020-05-03 14:45:34 -0400

    Add project files.

---
+++ MorseCodeMachine/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+
+namespace MorseCodeMachine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.ForegroundColor = ConsoleColor.Gray;
+
+                string output = "";
+
+                Console.WriteLine("Enter message");
+                Console.Write(": ");
+                string message = Console.ReadLine().ToLower();
+
+                string[] words = message.Split(' ');
+
+                foreach (string word in words)
+                {
+                    foreach (char letter in word)
+                    {
+                        string cs = @"Data Source=.\MorseCode.db";
+                        string sql = $"SELECT Code FROM MorseCode WHERE Character == '{letter}'";
+
+                        using (var connection = new SQLiteConnection(cs))
+                        {
+
+                            output += $"{connection.Query<string>(sql).FirstOrDefault()}";
+
+                        }
+
+                        output += "    ";
+                    }
+                    output += "    ";
+                }
+
+                Console.ForegroundColor = ConsoleColor.Green;
+
+                Console.WriteLine(output);
+
+                foreach (var item in output)
+                {
+                    if (item != ' ')
+                    {
+                        if (item == '.')
+                        {
+                            Console.Beep(700, 200);
+                            Task.Delay(240);
+                        }
+                        else if (item == '-')
+                        {
+                            Console.Beep(700, 700);
+                            Task.Delay(240);
+
+                        }
+                    }
+                }
+
+                Console.ReadKey(); 
+            }
+        }
+    }
+}
